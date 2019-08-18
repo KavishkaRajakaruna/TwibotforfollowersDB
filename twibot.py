ba467b92369ec611685dc63213dc067d8dee2471
Kavishka Hirushan <Kavishkanccmaths@gmail.com>
2019-08-18 23:09:03 +0530

    Database is used to store users & Ids

--- twibot.py
+++ twibot.py
@@ -1,5 +1,7 @@
 import tweepy
 import time
+import mysql.connector
+
 
 CONSUMER_KEY='yd93UC5uCzySNKQWN5HqRHn3b'
 CONSUMER_SECRET = 'ERX01c6IDIG1loMxHa2XPi3p6g8SViruXwoek5ZgOnKalQkSlq'
@@ -15,14 +17,33 @@ try:
 except:
 	print("Authentication Failed")
 
+db = mysql.connector.connect(
+	host ="localhost",
+	user ="usradmin",
+	passwd ="abc@123",
+	database ="twitusers"
+	)
+mycursor = db.cursor()
+
+sql="insert into users (UID,Name) values (%s,%s)"
+
+
 def usrwtxt():
 	for x in range(1,5):
 		
-		timelines = api.user_timeline(page = x)
+		timelines = api.home_timeline(page = x)
 			#print(timelines[0])
 
 		for timeline in timelines:
-			print(timeline.text+ '    -    ' + timeline.user.name)
+			#print(timeline.text+ '    -    ' + timeline.user.name)
+			val=(timeline.user.id,timeline.user.name)
+			print(val)
+			try:
+				mycursor.execute(sql,val)
+				db.commit()
+			except Exception:
+				print("database push error")
+
 
 while True:
 	usrwtxt()
