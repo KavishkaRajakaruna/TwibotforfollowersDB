0887aedf784b0074f653f74141051692bd501e88
Kavishka Hirushan <Kavishkanccmaths@gmail.com>
2019-08-18 22:35:13 +0530

    The Twitterbot for reading tweets upto 5 pages, display username & tweet

---
+++ twibot.py
@@ -0,0 +1,29 @@
+import tweepy
+import time
+
+CONSUMER_KEY='yd93UC5uCzySNKQWN5HqRHn3b'
+CONSUMER_SECRET = 'ERX01c6IDIG1loMxHa2XPi3p6g8SViruXwoek5ZgOnKalQkSlq'
+ACCESS_KEY = '1040889328069697536-cAZLhTDH8C70PuruHME5Vfo8r6zrY1'
+ACCESS_SECRET = 'L2kZLJIGBJI3qHbI2JLLnRDd5PDcYOQgTTlQRCxxmD0NJ'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+try:
+	api = tweepy.API(auth)
+	print("Authentication Success")
+except:
+	print("Authentication Failed")
+
+def usrwtxt():
+	for x in range(1,5):
+		
+		timelines = api.user_timeline(page = x)
+			#print(timelines[0])
+
+		for timeline in timelines:
+			print(timeline.text+ '    -    ' + timeline.user.name)
+
+while True:
+	usrwtxt()
+	time.sleep(10)
